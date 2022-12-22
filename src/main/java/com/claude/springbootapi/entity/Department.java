ed7cfca60f85b1f726c776a2a6770428e3d0ecb0
claudeomosa <claudeomosa@gmail.com>
2022-12-22 13:10:17 +0300

    refactor: remove boilerplate (getters and setters) code and refactor with the @Data annotation provided by lombok.Data

--- src/main/java/com/claude/springbootapi/entity/Department.java
+++ src/main/java/com/claude/springbootapi/entity/Department.java
@@ -5,8 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Department {
     @Id          //make departmentId the unique identifier of a Department entity
     @GeneratedValue(strategy = GenerationType.AUTO) //PK auto generated
@@ -16,55 +20,5 @@ public class Department {
     private String departmentSchool;
     private String departmentCode;
 
-    public long getDepartmentId() {
-        return departmentId;
-    }
 
-    public void setDepartmentId(long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentSchool() {
-        return departmentSchool;
-    }
-
-    public void setDepartmentSchool(String departmentSchool) {
-        this.departmentSchool = departmentSchool;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-
-    public Department(long departmentId, String departmentName, String departmentSchool, String departmentCode) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentSchool = departmentSchool;
-        this.departmentCode = departmentCode;
-    }
-
-    public Department() {
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentId=" + departmentId +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentSchool='" + departmentSchool + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
 }
