54694c2514c66fbda13c42b2815a03aa14343348
claudeomosa <claudeomosa@gmail.com>
2022-12-23 07:12:43 +0300

    add config in properties file

--- src/main/java/com/claude/springbootapi/controller/HelloController.java
+++ src/main/java/com/claude/springbootapi/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.claude.springbootapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${hello.world}")
+    private String welcomeMessage;
     @GetMapping("/")
     public String hello(){
-        return "hello charles";
+        return welcomeMessage;
     }
 }
